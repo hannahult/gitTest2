165f56060cf0912328f5e333f42f2759e5772fe2
Hanna Hult <hannaaaa31@gmail.com>
2024-10-31 10:17:34 +0100

    Added fine cululation

--- gitTest2/Program.cs
+++ gitTest2/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hejsan världen!");
+
+            Console.WriteLine("Böter: " + Helpers.CalculateFine(15, 5));
         }
     }
 }
